bc68aac2b31f2561b7e97e1ba0baa4b633580b2f
Todd Woodrow <tswoodrow@outlook.com>
2021-02-24 22:58:11 -0500

    multilang support

---
+++ earthboundhpgen/LANG.js
@@ -0,0 +1,35 @@
+//Supports language addition
+lang = {
+    libs :
+	[
+	"ifBrowserLang:en",
+			"Name",
+			"Initial HP number",
+			"Final HP number",
+			"Initial PP number",
+			"Final PP number",
+			"GENERATE",
+			"Download GIF",
+			"Successfully generated",
+			"Name has invalid letter",
+			"Enter a number",
+			"Number must be less than 4 digits",
+			"Style",
+			"Programmed by [v]",
+	"ifBrowserLang:es",
+			"Nombre",
+			"Número de PV inicial",
+			"Número de PV final",
+			"Número de PP inicial",
+			"Número de PP final",
+			"GENERAR",
+			"Descargar GIF",
+			"Generado exitosamente",
+			"Nombre tiene letra inválida",
+			"Introduce un número",
+			"Número debe ser de menos de 4 dígitos",
+			"Estilo",
+			"Programado por [v]",
+	""
+	]
+};
